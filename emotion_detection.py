9a9b1d549511f11af3067ab37a9b22c34ec1c1eb
TheKman100 <kylerstubbs@gmail.com>
2025-12-10 16:30:42 -0500

    Separate Emotion Values

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,13 +1,24 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
         # URL of the emotion detection service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-        # Create a dictionary with the text to be analysed
-    input_json = { "raw_document": { "text": text_to_analyse } }
-        # Set the headers required for the API request
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-        # Send a POST request to the API with text and headers
-    response = requests.post(url, json=input_json, headers=header)
-        # Return the response text from the API
-    return response.text
+    input_json = { "raw_document": { "text": text_to_analyse } }                            # Create a dictionary with the text to be analysed
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}     # Set the headers required for the API request
+    response = requests.post(url, json=input_json, headers=header)                          # Send a POST request to the API with text and headers
+    
+    #return response.text
+ 
+    formatted_response = json.loads(response.text)                                          # Parse the JSON response from the API
+        # Extract sentiment scores separately from the response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    joy_score = emotions['joy']
+    fear_score = emotions['fear']
+    sadness_score = emotions['sadness']
+    disgust_score = emotions['disgust']
+    dict_emotion = {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score}
+    dom_emotion = max(dict_emotion, key=dict_emotion.get)
+        # Return dictionary of responses from the API
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dom_emotion}
